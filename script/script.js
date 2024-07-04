bcb4a4f37ac52ad60d74c041c3813b6e82d27df3
fahrul syawal <fahrul@gmail.com>
2024-07-04 15:56:59 +0900

    Buat search bar interactive

--- script/script.js
+++ script/script.js
@@ -1,15 +1,28 @@
-// Toggle class active
+// Toggle class active hamburger menu
 const navbarNav = document.querySelector('.navbar-nav');
-
 const hamburger = document.querySelector('#hamburger-menu');
+const searhButton = document.querySelector('#search-button');
 
 hamburger.addEventListener('click', () => {
   navbarNav.classList.toggle('active');
 });
 
-// Klik di luar sidebar untuk menghilangkan navbar
+//Toggle class active search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Klik di luar elemn
 document.addEventListener('click', (e) => {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove('active');
+  }
+  if (!searhButton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active');
   }
 });
